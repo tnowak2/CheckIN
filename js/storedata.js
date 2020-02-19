5b065e152a2ec83c294c5b3a6ddaf720e1263dca
chzeng03 <60713966+chzeng03@users.noreply.github.com>
2020-02-19 16:04:30 -0500

    Add files via upload

---
+++ js/storedata.js
@@ -0,0 +1,18 @@
+﻿var db = firebase.firestore();
+
+function storeData() {
+    var inputTextemail = document.getElementById("signUpemail").value;
+    var inputTextpass = document.getElementById("signUppass").value
+
+    db.collection("users").doc.add({
+        pass: inputTextpass,
+        email: inputTextemail
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+
+};
